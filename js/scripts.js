73db1e8ab460a7d3806e3fe6b0ac39e415f39601
Filipe Correia <f7lipe@Me.com>
2022-01-30 08:38:51 -0300

    feat: verify
    Essa feature apresenta uma função que verifica se três ítems foram selecionados entre as categorias e libera o botão de fechar a compra.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-function selectDish(id){
-    let selection = document.querySelector('.dishes .dish .selection')
+let dishPrice = 0
+let drinkPrice = 0
+let dessertPrice = 0
+
+function selectDish(id, priceID){
+    const selection = document.querySelector('.dishes .dish .selection')
     if (selection !== null){
         selection.classList.remove('selection')
     }
 
-    let selected = document.getElementById(id)
+    const selected = document.getElementById(id)
     selected.classList.add('selection')
+
+    dishPrice = document.getElementById(priceID).innerHTML.replace('R$:', '').replace(',', '.')
+    verify()
 }
 
-function selectDrink(id){
+function selectDrink(id, priceID){
     let selection = document.querySelector('.drinks .drink .selection')
     if (selection !== null){
         selection.classList.remove('selection')
@@ -16,9 +23,12 @@ function selectDrink(id){
 
     let selected = document.getElementById(id)
     selected.classList.add('selection')
+
+    drinkPrice = document.getElementById(priceID).innerHTML.replace('R$:', '').replace(',', '.')
+    verify()
 }
 
-function selectDessert(id){
+function selectDessert(id, priceID){
     let selection = document.querySelector('.desserts .dessert .selection')
     if (selection !== null){
         selection.classList.remove('selection')
@@ -26,5 +36,17 @@ function selectDessert(id){
 
     let selected = document.getElementById(id)
     selected.classList.add('selection')
+
+    dessertPrice = document.getElementById(priceID).innerHTML.replace('R$:', '').replace(',', '.')
+    verify()
+}
+
+function verify(){
+    const finishBtn = document.querySelector('.finish-btn')
+
+    if((dishPrice !==0) &&( drinkPrice !==0) && (dessertPrice !==0)){
+        finishBtn.innerHTML = 'Fechar pedido'
+        finishBtn.disabled = false
+    }
 }
 
